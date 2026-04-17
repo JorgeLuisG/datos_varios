827184b5c1091d7fd047927e012f00d6677bc237
JL.Garcia <JL.Garcia@edemsa1.local>
2026-04-17 11:17:59 -0300

    login

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 import streamlit as st
 from search import cargar_datos, buscar
+USUARIOS = {
+    "admin": "pass0",
+    "Gurbano": "pass1",
+    "isora": "pass2"
+}
 
+def login():
+    st.title("Login")
+
+    usuario = st.text_input("Usuario")
+    password = st.text_input("Contraseña", type="password")
+
+    if st.button("Ingresar"):
+        if usuario in USUARIOS and USUARIOS[usuario] == password:
+            st.session_state["login"] = True
+            st.session_state["usuario"] = usuario
+            st.rerun()
+        else:
+            st.error("Usuario o contraseña incorrectos")
 st.title("Buscador de Tablas")
+st.sidebar.write(f"Usuario: {st.session_state['usuario']}")
+if "login" not in st.session_state:
+    st.session_state["login"] = False
 
+if not st.session_state["login"]:
+    login()
+    st.stop()
+st.sidebar.write(f"Usuario: {st.session_state['usuario']}")
 # cargar datos
 cuil_contratista, contratos, cronograma_poda, certificacion_poda = cargar_datos()
 
