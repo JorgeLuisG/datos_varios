c2bfa02345834b1c3c17c16e82b4d36f1c8fdd0c
JorgeLuisG <jorgeluisgarciajlg@gmail.com>
2026-06-01 14:55:45 -0300

    base de datos compartida

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def get_connection():
-    DATABASE_URL = "postgresql://organizador_db_ahov_user:pd07nJbb0QuBfZAip9tDYH3rVKJZ8sZk@dpg-d7h4v9po3t8c7396v6g0-a.oregon-postgres.render.com/organizador_db_ahov"
-
     return psycopg2.connect(
-        DATABASE_URL,
-        sslmode="require"
+        os.getenv("DATABASE_URL")
     )
