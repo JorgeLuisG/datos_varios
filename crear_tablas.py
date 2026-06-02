890f32ab2c4d2a63368f00ac075fb5a407eea2f0
JorgeLuisG <jorgeluisgarciajlg@gmail.com>
2026-06-02 15:45:56 -0300

    mensaje4

--- crear_tablas.py
+++ crear_tablas.py
@@ -1,21 +1,19 @@
 from db import get_connection
 
-conn = get_connection()
-cur = conn.cursor()
+def crear_tabla_usuarios():
+    conn = get_connection()
+    cur = conn.cursor()
 
-cur.execute("""
-CREATE TABLE IF NOT EXISTS usuarios (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    nombre VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    rol VARCHAR(50) NOT NULL
-);
-""")
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        nombre VARCHAR(100) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        rol VARCHAR(50) NOT NULL
+    );
+    """)
 
-conn.commit()
-
-cur.close()
-conn.close()
-
-print("Tabla usuarios creada")
+    conn.commit()
+    cur.close()
+    conn.close()
